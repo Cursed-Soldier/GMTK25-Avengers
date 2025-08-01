6e85b3b6e0714008df001c376282e27ded789d4d
Daniel Kromek <kromekdani@gmail.com>
2025-08-01 22:25:18 +0300

    I want to end it all

--- GMTK25-Avengers-Game/Assets/Scripts/LoopTeleport.cs
+++ GMTK25-Avengers-Game/Assets/Scripts/LoopTeleport.cs
@@ -8,16 +8,18 @@ public class LoopTeleport : MonoBehaviour
   public float screenHalfWidth = 7.2f;
   GameObject player;
   GameObject wall;
+  WallMovement wallMovement;
 
     public void Awake()
     {
       player = GameObject.FindGameObjectWithTag("Player");
       wall = GameObject.FindGameObjectWithTag("Wall");
+      wallMovement = wall.GetComponent<WallMovement>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision){
 
-      if(collision.CompareTag("Player") || collision.CompareTag("Wall")){
+      if(collision.CompareTag("Player")){
         
         Transform obj = collision.transform;
         Vector2 pos = obj.position;
@@ -28,9 +30,24 @@ public class LoopTeleport : MonoBehaviour
         else if (teleportEdge == Edge.Left){
           pos.x = screenHalfWidth - 1f;
         }
-
         obj.position = pos;
       }
+
+        if (collision.CompareTag("Wall")){
+                  
+        Transform obj = collision.transform;
+        Vector2 pos = obj.position;
+
+        if (teleportEdge == Edge.Right){
+          pos.x = -screenHalfWidth + 1f;
+          wallMovement.StartDragging(true);
+        }
+        else if (teleportEdge == Edge.Left){
+          pos.x = screenHalfWidth - 1f;
+          wallMovement.StartDragging(false);
+        }
+        obj.position = pos;
+        }
     }
 }
 
--- GMTK25-Avengers-Game/Assets/Scripts/WallMovement.cs
+++ GMTK25-Avengers-Game/Assets/Scripts/WallMovement.cs
@@ -1,17 +1,24 @@
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class WallMovement : MonoBehaviour
 {
     public InputAction MoveAction;
-
     private Rigidbody2D rb;
 
     public float moveSpeed = 10f;
 
+    public LevelManager levelManager;
+
+    private Transform draggedLevel = null;
+    private int draggedIndex = 1;
+    private bool movingForward = true;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        draggedLevel = levelManager.GetLevel(draggedIndex);
     }
 
     void OnEnable()
@@ -28,5 +35,40 @@ public class WallMovement : MonoBehaviour
     {
         Vector2 input = MoveAction.ReadValue<Vector2>();
         rb.linearVelocity = new Vector2(input.x * moveSpeed, rb.linearVelocity.y);
+
+        if (input.x != 0)
+            movingForward = input.x > 0;
+
+        if (draggedLevel != null)
+        {
+            draggedLevel.position += Vector3.right * input.x * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    public void StartDragging(bool rightLoop)
+    {
+        // This needs a lot of tweaking and edge case handling, I'm geeking out so I commit
+        if (rightLoop)
+        {
+            levelManager.DisableLevel(draggedIndex - 1);
+            levelManager.SetActiveLevel(draggedIndex);
+            levelManager.SnapLevelToCenter(draggedLevel);
+            draggedIndex++;
+            draggedLevel = levelManager.GetLevel(draggedIndex);
+            levelManager.SnapNextLevelToLeft(draggedLevel);
+        }
+
+        else
+        {
+            draggedIndex--;
+            draggedLevel = levelManager.GetLevel(draggedIndex);
+            levelManager.EnableLevel(draggedIndex);
+            levelManager.SetActiveLevel(draggedIndex);
+            levelManager.SnapLevelToCenter(draggedLevel);
+            draggedLevel = levelManager.GetLevel(draggedIndex + 1);
+            levelManager.SnapNextLevelToRight(draggedLevel);
+            UnityEngine.Debug.Log("Left");
+            UnityEngine.Debug.Log(draggedIndex);
+        }
     }
 }
